9b9bdb842c1ab48e7e4faa461b60d9f461ee134d
Consolata Njeri <conso4u@gmail.com>
2021-10-23 17:08:24 -0700

    starter code

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+
+  return (
+    <section class="sectionRow" id="contact-me">
+        <div class="columnAlignment">
+            <ul>
+                <li><a href="https://github.com/Conso97" target="_blank">206.557.0388</a></li>
+                <li><a href="https://conso97.github.io/" target="_blank">Conso4u@website.com</a></li>
+                <li><a href="https://github.com/Conso97" target="_blank">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/conso97/" target="_blank">LinkedIn</a></li>
+                <li><a href="mailto:conso4u@gmail.com/"target="Gmail">Conso4u@gmail.com</a></li>
+            </ul>
+        </div>
+    </section>
+  );
+};
+
+export default Footer;
